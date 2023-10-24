fa6ee4777f5ed24ac0b8f91d1e75e88724cd969a
Duesc <minhduchalai4@gmail.com>
2023-10-24 15:58:12 +0700

    reset

--- DeeDictionary/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module main.deedictionary {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens main.deedictionary to javafx.fxml;
-    exports main.deedictionary;
-}

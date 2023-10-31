cb83a63ba1e9ddc43ff90ae925a060ce2b6938da
Duesc <minhduchalai4@gmail.com>
2023-11-01 05:28:55 +0700

    remain

--- DeeDictionaryApplication/src/main/java/deedictionaryapplication/Main.java
+++ DeeDictionaryApplication/src/main/java/deedictionaryapplication/Main.java
@@ -4,8 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Main extends Application {
@@ -16,6 +16,7 @@ public class Main extends Application {
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.setTitle("Dee Dictionary");
+            stage.getIcons().add(new Image("file:src/main/resources/Icons/title.png"));
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
